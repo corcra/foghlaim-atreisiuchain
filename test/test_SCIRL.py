ab491f917c95491ab24bc5a4d4c11174ae67a4e2
Stephanie Hyland <steph.hyland@gmail.com>
2015-06-18 17:38:40 -0400

    use available MDP

--- test/test_SCIRL.py
+++ test/test_SCIRL.py
@@ -13,13 +13,15 @@ mdp_path = './data/test_N100_MDP.npy'
 # --- get data --- #
 trajectory, basis_functions = SCIRL.get_training_data(traj_path, basis_path)
 
-# --- run SCIRL --- #
-SCIRL_reward = SCIRL.SCIRL(trajectory, basis_functions, n_actions=4)
-
 # --- load true MDP --- #
 mdp = np.load(mdp_path).item()
 MDP_reward = mdp.rewards
 
+# --- run SCIRL --- #
+SCIRL_reward_modelfree = SCIRL.SCIRL(trajectory, basis_functions, n_actions=4)
+SCIRL_reward_model = SCIRL.SCIRL(trajectory, basis_functions, n_actions=4,mdp=mdp)
+
 # --- compare --- #
-print 'SCIRL rewards:', SCIRL_reward
+print 'SCIRL rewards: (no model)', SCIRL_reward_modelfree
+print 'SCIRL rewards: (model)', SCIRL_reward_model
 print 'MDP rewards:', MDP_reward
