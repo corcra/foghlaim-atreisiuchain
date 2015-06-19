c0ce08c3b0ca7d0157babe23a35567b53f17ae83
Stephanie Hyland <steph.hyland@gmail.com>
2015-06-19 16:43:22 -0400

    generating test training data in a better way >.>

--- test/test_CLF.py
+++ test/test_CLF.py
@@ -7,20 +7,30 @@ import numpy as np
 # -- make features -- #
 A = 3
 S = 10
-k = 6
+k = 7
 # not very reasonable
-features = np.random.normal(size=(A, S, k))
+features = np.random.normal(size=(A, S, k))         # |A| x |S| x k (duh)
 
 # -- define clf -- #
 clf = clf_large_margin(features)
 
 # -- make data -- #
-N = 1000
+N = 100000
 states = np.random.randint(low=0, high=S, size=N)
-# trivial sort of classification
-labels = states % 2
-#labels = (states + np.random.binomial(S, p=0.05, size=N)) % A
+# fake some 'true' parameters
+true_params = np.random.normal(size=k)
+true_score_fn = np.dot(features, true_params)       # |A| x |S|
+# use to predict labels
+scores = true_score_fn[:, states]                   # |A| x N
+labels = np.argmax(scores, axis=0)
+# combine
 data = np.vstack((states, labels)).transpose()
 
 # -- fit ! -- #
 clf.fit(data)
+
+# --- compare true params --- #
+print 'True objective:', clf.objective(data, lambd=0.5, params=true_params)
+print 'Fitted objective:', clf.objective(data, lambd=0.5)
+print 'True parameters:', true_params
+print 'Fitted parameters:', clf.params
